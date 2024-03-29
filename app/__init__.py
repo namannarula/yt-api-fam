d43db97e5d1f1b08e147f4d206e554246ade591d
Naman Narula <naman@narula.dev>
2024-03-29 22:06:41 +0530

    redis init

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
+import redis
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
+
 
 from app.models import video_model  # Import the Video model
 
